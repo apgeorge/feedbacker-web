a5c2ae5d2af7c418b90f6177e2c3fe38d201d466
aroj <apgeorge@thoughtworks.com>
2014-05-12 15:31:10 +0530

    Fixing jquery requirjs integration

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 require.config({
     paths: {
-        jquery: '../bower_components/jquery/jquery'
+        jquery: '../bower_components/jquery/dist/jquery'
     },
     shim: {
         bootstrap: {
